e6480f0eab38f1a2ad735a4740bd0696dec6b66f
Replit user <>
2023-09-16 12:36:04 +0000

    format lock_modules.py

--- scripts/lock_modules.py
+++ scripts/lock_modules.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # lock_modules.py
 
 # This script generates/updates the module registry file `modules.json`.
@@ -39,63 +40,74 @@ import os
 import re
 import argparse
 
-module_registry_id_regex = re.compile(r'^(.+):v([0-9]+)-([0-9]+)-([0-9a-z]+)$')
-module_registry_file = 'modules.json'
-nix_flags = ['--extra-experimental-features', 'nix-command flakes discard-references']
+module_registry_id_regex = re.compile(r"^(.+):v([0-9]+)-([0-9]+)-([0-9a-z]+)$")
+module_registry_file = "modules.json"
+nix_flags = ["--extra-experimental-features", "nix-command flakes discard-references"]
+
 
 def get_commit_info():
-  output = subprocess.check_output(['git', 'show', '-s', '--format=format:%as|%H'])
-  date, commit = str(output, 'UTF-8').split('|')
-  return {
-    'sha': commit,
-    'date': date.replace('-', '')
-  }
+  output = subprocess.check_output(["git", "show", "-s", "--format=format:%as|%H"])
+  date, commit = str(output, "UTF-8").split("|")
+  return {"sha": commit, "date": date.replace("-", "")}
+
 
 def is_working_directory_clean():
-  output = subprocess.check_output(['git', 'status', '--porcelain', '--untracked-files=no'])
+  output = subprocess.check_output(
+    ["git", "status", "--porcelain", "--untracked-files=no"]
+  )
   return len(output) == 0
 
+
 def get_current_modules():
-  output = subprocess.check_output(['nix', 'eval', '.#modules', '--json'] + nix_flags)
+  output = subprocess.check_output(["nix", "eval", ".#modules", "--json"] + nix_flags)
   return json.loads(output)
+
 
 def get_module_registry():
   if not os.path.isfile(module_registry_file):
     return {}
-  f = open(module_registry_file, 'r')
+  f = open(module_registry_file, "r")
   fromfile = json.load(f)
   f.close()
   registry = {}
   for module_registry_id, entry in fromfile.items():
     match = module_registry_id_regex.match(module_registry_id)
+    assert match
     module_id, version, date, commit = match.groups()
     if module_id not in registry:
       registry[module_id] = []
-    registry[module_id].append({
-      'id': module_id,
-      'version': int(version),
-      'date': date,
-      'commit': entry['commit'],
-      'path': entry['path']
-    })
+    registry[module_id].append(
+      {
+        "id": module_id,
+        "version": int(version),
+        "date": date,
+        "commit": entry["commit"],
+        "path": entry["path"],
+      }
+    )
   return registry
 
+
 def save_module_registry(registry):
-  f = open(module_registry_file, 'w')
+  f = open(module_registry_file, "w")
   mapping = {}
   for module_id, versions in registry.items():
     for version in versions:
       registry_id = get_module_registry_id(version)
-      mapping[registry_id] = {
-        'commit': version['commit'],
-        'path': version['path']
-      }
-  json.dump(mapping, f, indent = 2)
+      mapping[registry_id] = {"commit": version["commit"], "path": version["path"]}
+  json.dump(mapping, f, indent=2)
   f.close()
-  print('Wrote %s' % module_registry_file)
+  print("Wrote %s" % module_registry_file)
+
 
 def get_module_registry_id(module):
-  return "%s:v%d-%s-%s" % (module['id'], module['version'], module['date'], module['commit'][:7])
+  return "%s:v%d-%s-%s" % (
+    module["id"],
+    module["version"],
+    module["date"],
+    module["commit"][:7],
+  )
+
 
 def update_module_registry(module_registry):
   commit = get_commit_info()
@@ -106,53 +118,62 @@ def update_module_registry(module_registry):
     if module_id in module_registry:
       # check for module update
       current = module_registry[module_id][-1]
-      if current['path'] == module_path:
+      if current["path"] == module_path:
         continue
 
     if module_id not in module_registry:
       module_registry[module_id] = []
-    
+
     numeric_version = 1 + len(module_registry[module_id])
     module = {
-      'id': module_id,
-      'version': numeric_version,
-      'date': commit['date'],
-      'commit': commit['sha'],
-      'path': module_path
+      "id": module_id,
+      "version": numeric_version,
+      "date": commit["date"],
+      "commit": commit["sha"],
+      "path": module_path,
     }
     registry_id = get_module_registry_id(module)
     module_registry[module_id].append(module)
     if current:
-      print('%s -> %s' % (get_module_registry_id(current), registry_id))
+      print("%s -> %s" % (get_module_registry_id(current), registry_id))
     else:
-      print('%s added' % registry_id)
+      print("%s added" % registry_id)
     changed = True
   return changed
-  
+
+
 def main():
   parser = argparse.ArgumentParser(
-    prog='lock_modules',
-    description='upserts current modules to %s' % module_registry_file,
+    prog="lock_modules",
+    description="upserts current modules to %s" % module_registry_file,
   )
 
-  parser.add_argument('-d', '--dirty', action='store_true', help='allow dirty working dir')
-  parser.add_argument('-v', '--verify', action='store_true', help='verify %s is up to date' % module_registry_file)
+  parser.add_argument(
+    "-d", "--dirty", action="store_true", help="allow dirty working dir"
+  )
+  parser.add_argument(
+    "-v",
+    "--verify",
+    action="store_true",
+    help="verify %s is up to date" % module_registry_file,
+  )
 
   args = parser.parse_args()
 
   if not args.dirty and not is_working_directory_clean():
-    print('There are uncommitted changes. Exiting.')
+    print("There are uncommitted changes. Exiting.")
     exit(1)
 
   module_registry = get_module_registry()
   changed = update_module_registry(module_registry)
 
   if args.verify and changed:
-    print('%s is not up to date!!' % module_registry_file)
+    print("%s is not up to date!!" % module_registry_file)
     exit(1)
 
   if changed:
     save_module_registry(module_registry)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
   main()
